aacabd9d29990aa4ea228d9c79912d2520899992
Matt Butcher <technosophos@gmail.com>
2011-12-19 13:19:46 -0600

    Added auth testing code.

---
+++ test/AuthTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file
+ * This is a simple command-line test for authentication.
+ *
+ * You can run the test with `php test/AuthTest.php username key`.
+ */
+
+require_once 'src/HPCloud/Transport/Transporter.php';
+require_once 'src/HPCloud/Transport/PHPStreamTransport.php';
+
+$usage = "php $0 ID KEY URL";
+
+if ($argc < 4) {
+  print 'ID, Key, and URL are all required.' . PHP_EOL;
+  print $usage . PHP_EOL;
+  exit(1);
+}
+
+$user = $argv[1];
+$key = $argv[2];
+$uri = $argv[3];
+
+$headers = array(
+  'X-Auth-User' => $user,
+  'X-Auth-Key' => $key,
+);
+
+$t = new \HPCloud\Transport\PHPStreamTransport();
+$res = $t->doRequest($uri, 'GET', $headers);
+
+
+//print_r($res);
