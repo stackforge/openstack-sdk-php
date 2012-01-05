2efb44ef6b2684dd1b2070bbd3de800d0a8ca705
Matt Butcher <technosophos@gmail.com>
2012-01-05 16:37:35 -0600

    Added exception for read-only objects.

---
+++ src/HPCloud/Storage/ObjectStorage/ReadOnlyObjectException.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ */
+namespace HPCloud\Storage\ObjectStorage;
+/**
+ * Thrown if an object that is read only is modified.
+ */
+class ReadOnlyObjectException extends \HPCloud\Exception {}
