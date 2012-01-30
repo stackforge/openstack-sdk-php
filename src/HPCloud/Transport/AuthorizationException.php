3526a2b1df5f76ecb649e79291ddec42dff6073e
Matt Butcher <technosophos@gmail.com>
2012-01-30 10:23:39 -0600

    Updated docs for AuthorizationException.

--- src/HPCloud/Transport/AuthorizationException.php
+++ src/HPCloud/Transport/AuthorizationException.php
@@ -1,9 +1,14 @@
 <?php
 /**
  * @file
+ *
+ * The authorization exception.
  */
 namespace HPCloud\Transport;
 /**
- * Represents an HTTP 401.
+ * Thrown when an access constraint is not met.
+ *
+ * Represents an HTTP 401 or 403 exception. In the future,
+ * 401 and 403 will each have their own exceptions.
  */
 class AuthorizationException extends \HPCloud\Exception {}
