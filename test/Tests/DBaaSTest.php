31b5646bc5c42e1a1ad42c8f804c201a39786dbf
Matt Butcher <technosophos@gmail.com>
2012-05-14 17:27:19 -0500

    Initial add of DBaaS class and test.

---
+++ test/Tests/DBaaSTest.php
@@ -0,0 +1,57 @@
+<?php
+/* ============================================================================
+(c) Copyright 2012 Hewlett-Packard Development Company, L.P.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights to
+use, copy, modify, merge,publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+============================================================================ */
+/**
+ * @file
+ *
+ * Unit tests for DBaaS.
+ */
+namespace HPCloud\Tests\Services;
+
+require_once 'src/HPCloud/Bootstrap.php';
+require_once 'test/TestCase.php';
+
+use \HPCloud\Services\DBaaS;
+
+class DBaaSTest extends \HPCloud\Tests\TestCase {
+
+  public function testNewFromServiceCatalog() {
+    $this->markTestIncomplete();
+  }
+
+  public function testConstructor() {
+    $this->markTestIncomplete();
+  }
+
+  /**
+   * @depends testConstructor
+   */
+  public function testInstance() {
+    $this->markTestIncomplete();
+  }
+
+  /**
+   * @depends testConstructor
+   */
+  public function testSnapshot() {
+    $this->markTestIncomplete();
+  }
+}
