e8228ea2fc51cd9cd4ebfac2ae8e1952a2604207
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-06-12 13:33:16 +0200

    Re-formatting and adding Rackspace to Copyright headers
    Previously, all work submitted by Rackspace employees were done with
    "Copyright OpenStack Foundation" headers. This was done because we were
    uncertain about the correct legal solution; since then, we have been
    instructed to label all our work with "Rackspace US, Inc." copyrights
    instead. This commit does that and reformats the docblock to a more
    consistent style. Other PHP projects like Symfony, Composer, Doctrine,
    etc. all do this.
    
    Change-Id: I3e03b624d889a7a687ce630feef918ef9a24d015

--- src/OpenStack/ObjectStore/v1/ObjectStorage.php
+++ src/OpenStack/ObjectStore/v1/ObjectStorage.php
@@ -1,26 +1,20 @@
 <?php
-/* ============================================================================
-(c) Copyright 2012-2014 Hewlett-Packard Development Company, L.P.
 
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-
-             http://www.apache.org/licenses/LICENSE-2.0
-
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
-============================================================================ */
-/**
- * This file provides the ObjectStorage class, which is the primary
- * representation of the ObjectStorage system.
+/*
+ * (c) Copyright 2012-2014 Hewlett-Packard Development Company, L.P.
+ * (c) Copyright 2014      Rackspace US, Inc.
  *
- * ObjectStorage (aka Swift) is the OpenStack service for providing
- * storage of complete and discrete pieces of data (e.g. an image file,
- * a text document, a binary).
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 
 namespace OpenStack\ObjectStore\v1;
